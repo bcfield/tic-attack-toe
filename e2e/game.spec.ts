a75d4cbcb687291ea559147d95a33aed2f89f3f8
Brett Field <brett@digitalclarity.co>
2024-07-28 17:42:18 +0200

    Playwright config update.

--- e2e/game.spec.ts
+++ e2e/game.spec.ts
@@ -1,178 +1,174 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Tic Attack Toe App', () => {
+test.beforeEach(async ({ page }) => {
 
-  test.beforeEach(async ({ page }) => {
+  // Open page
+  await page.goto('http://127.0.0.1:5500');
 
-    // Open page
-    await page.goto('http://127.0.0.1:5500');
+  // Click on the begin button
+  await page.click('#button-begin');
 
-    // Click on the begin button
-    await page.click('#button-begin');
+  // Enter player 1 details after animation
+  await page.waitForTimeout(1200);
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.click('#player1-colors .color-circle.bg-plum');
 
-    // Enter player 1 details after animation
-    await page.waitForTimeout(1200);
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.click('#player1-colors .color-circle.bg-plum');
+  // Enter player 2 details after animation
+  await page.waitForTimeout(1200);
+  await page.fill('#player2-name-input', 'Player 2');
+  await page.click('#player2-colors .color-circle.bg-peach');
 
-    // Enter player 2 details after animation
-    await page.waitForTimeout(1200);
-    await page.fill('#player2-name-input', 'Player 2');
-    await page.click('#player2-colors .color-circle.bg-peach');
+  // Select zen mode
+  await page.waitForTimeout(1200);
+  await page.click('#button-start-zen');
 
-    // Select zen mode
-    await page.waitForTimeout(1200);
-    await page.click('#button-start-zen');
+  // Wait for countdown to complete after animation
+  await page.waitForTimeout(6200);
+  
+});
 
-    // Wait for countdown to complete after animation
-    await page.waitForTimeout(6200);
-    
-  });
+test('Should place an X or O in the block', async ({ page }) => {
+  const cells = page.locator('.board-cell');
 
-  test('Should place an X or O in the block', async ({ page }) => {
-    const cells = page.locator('.board-cell');
+  // Detect which player was randomly selected to go first
+  await cells.nth(0).click();
+  const firstSymbol = (await cells.nth(0).textContent())!;
+  const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
 
-    // Detect which player was randomly selected to go first
-    await cells.nth(0).click();
-    const firstSymbol = (await cells.nth(0).textContent())!;
-    const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
+  // Confirm symbol of second move
+  await cells.nth(1).click();
+  await expect(cells.nth(1)).toHaveText(secondSymbol);
 
+  // Confirm symbol of third move
+  await cells.nth(2).click();
+  await expect(cells.nth(2)).toHaveText(firstSymbol);
+});
+
+
+test('Should identify a winner', async ({ page }) => {
+  const cells = page.locator('.board-cell');
+
+  // Detect which player was randomly selected to go first
+  await cells.nth(0).click();
+  const firstSymbol = (await cells.nth(0).textContent())!;
+  const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
+
+  // Play a winning game scenario
+  await cells.nth(3).click();
+  await cells.nth(1).click();
+  await cells.nth(4).click();
+  await cells.nth(2).click();
+
+  // Check winner text based on the first symbol placed
+  const winner = firstSymbol === 'X' ? 'Player 1' : 'Player 2';
+  await expect(page.locator('#turn-indicator')).toHaveText(`${winner} wins!`);
+});
+
+test('Should identify a draw', async ({ page }) => {
+  const cells = page.locator('.board-cell');
+
+  // Play a draw scenario
+  await cells.nth(0).click();
+  await cells.nth(1).click();
+  await cells.nth(2).click();
+  await cells.nth(4).click();
+  await cells.nth(3).click();
+  await cells.nth(5).click();
+  await cells.nth(7).click();
+  await cells.nth(6).click();
+  await cells.nth(8).click();
+  
+  // Check draw text is displayed
+  await expect(page.locator('#turn-indicator')).toHaveText("It's a draw!");
+});
+
+test('Should reset the game', async ({ page }) => {
+  const cells = page.locator('.board-cell');
+  const resetButton = page.locator('#button-reset-zen');
+
+  // Play two moves
+  await cells.nth(0).click();
+  await cells.nth(1).click();
+
+  // Click on the reset button
+  await resetButton.click();
+
+  // Check played cells are empty
+  await expect(cells.nth(0)).toHaveText('');
+  await expect(cells.nth(1)).toHaveText('');
+});
+
+test('Should handle keybindings correctly', async ({ page }) => {
+  const cells = page.locator('.board-cell');
+
+  await cells.nth(0).click();
+  const firstSymbol = (await cells.nth(0).textContent())!;
+  const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
+
+  if (firstSymbol === 'X') {
     // Confirm symbol of second move
-    await cells.nth(1).click();
+    await page.keyboard.press('o');
     await expect(cells.nth(1)).toHaveText(secondSymbol);
-
     // Confirm symbol of third move
-    await cells.nth(2).click();
+    await page.keyboard.press('e');
     await expect(cells.nth(2)).toHaveText(firstSymbol);
-  });
+  } else {
+    await page.keyboard.press('w');
+    await expect(cells.nth(1)).toHaveText(secondSymbol);
+    // Confirm symbol of third move
+    await page.keyboard.press('p');
+    await expect(cells.nth(2)).toHaveText(firstSymbol);
+  }
+});
 
+test('Should ensure players cannot make moves for each other', async ({ page }) => {
+  const cells = page.locator('.board-cell');
 
-  test('Should identify a winner', async ({ page }) => {
-    const cells = page.locator('.board-cell');
+  // Detect which player was randomly selected to go first
+  await cells.nth(0).click();
+  const firstSymbol = (await cells.nth(0).textContent())!;
+  const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
 
-    // Detect which player was randomly selected to go first
-    await cells.nth(0).click();
-    const firstSymbol = (await cells.nth(0).textContent())!;
-    const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
-
-    // Play a winning game scenario
-    await cells.nth(3).click();
-    await cells.nth(1).click();
-    await cells.nth(4).click();
-    await cells.nth(2).click();
-
-    // Check winner text based on the first symbol placed
-    const winner = firstSymbol === 'X' ? 'Player 1' : 'Player 2';
-    await expect(page.locator('#turn-indicator')).toHaveText(`${winner} wins!`);
-  });
-
-  test('Should identify a draw', async ({ page }) => {
-    const cells = page.locator('.board-cell');
-
-    // Play a draw scenario
-    await cells.nth(0).click();
-    await cells.nth(1).click();
-    await cells.nth(2).click();
-    await cells.nth(4).click();
-    await cells.nth(3).click();
-    await cells.nth(5).click();
-    await cells.nth(7).click();
-    await cells.nth(6).click();
-    await cells.nth(8).click();
-    
-    // Check draw text is displayed
-    await expect(page.locator('#turn-indicator')).toHaveText("It's a draw!");
-  });
-
-  test('Should reset the game', async ({ page }) => {
-    const cells = page.locator('.board-cell');
-    const resetButton = page.locator('#button-reset-zen');
-
-    // Play two moves
-    await cells.nth(0).click();
-    await cells.nth(1).click();
-
-    // Click on the reset button
-    await resetButton.click();
-
-    // Check played cells are empty
-    await expect(cells.nth(0)).toHaveText('');
+  if (firstSymbol === 'X') {
+    // Confirm player 1 false click
+    await page.keyboard.press('w');
     await expect(cells.nth(1)).toHaveText('');
-  });
 
-  test('Should handle keybindings correctly', async ({ page }) => {
-    const cells = page.locator('.board-cell');
+    // Player 2 true click
+    await page.keyboard.press('o');
 
-    await cells.nth(0).click();
-    const firstSymbol = (await cells.nth(0).textContent())!;
-    const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
+    // Confirm player 2 false click
+    await page.keyboard.press('p');
+    await expect(cells.nth(2)).toHaveText('');
+  } else {
+    // Confirm player 2 false click
+    await page.keyboard.press('o');
+    await expect(cells.nth(1)).toHaveText('');
 
-    if (firstSymbol === 'X') {
-      // Confirm symbol of second move
-      await page.keyboard.press('o');
-      await expect(cells.nth(1)).toHaveText(secondSymbol);
-      // Confirm symbol of third move
-      await page.keyboard.press('e');
-      await expect(cells.nth(2)).toHaveText(firstSymbol);
-    } else {
-      await page.keyboard.press('w');
-      await expect(cells.nth(1)).toHaveText(secondSymbol);
-      // Confirm symbol of third move
-      await page.keyboard.press('p');
-      await expect(cells.nth(2)).toHaveText(firstSymbol);
-    }
-  });
+    // Player 1 true click
+    await page.keyboard.press('w');
 
-  test('Should ensure players cannot make moves for each other', async ({ page }) => {
-    const cells = page.locator('.board-cell');
+    // Confirm player 1 false click
+    await page.keyboard.press('e');
+    await expect(cells.nth(2)).toHaveText('');
+  }
+});
 
-    // Detect which player was randomly selected to go first
-    await cells.nth(0).click();
-    const firstSymbol = (await cells.nth(0).textContent())!;
-    const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
+test('Should ensure sybols cannot be placed after a game is complete', async ({ page }) => {
+  const cells = page.locator('.board-cell');
 
-    if (firstSymbol === 'X') {
-      // Confirm player 1 false click
-      await page.keyboard.press('w');
-      await expect(cells.nth(1)).toHaveText('');
+  // Detect which player was randomly selected to go first
+  await cells.nth(0).click();
+  const firstSymbol = (await cells.nth(0).textContent())!;
+  const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
 
-      // Player 2 true click
-      await page.keyboard.press('o');
+  // Play a winning game scenario
+  await cells.nth(3).click();
+  await cells.nth(1).click();
+  await cells.nth(4).click();
+  await cells.nth(2).click();
 
-      // Confirm player 2 false click
-      await page.keyboard.press('p');
-      await expect(cells.nth(2)).toHaveText('');
-    } else {
-      // Confirm player 2 false click
-      await page.keyboard.press('o');
-      await expect(cells.nth(1)).toHaveText('');
-
-      // Player 1 true click
-      await page.keyboard.press('w');
-
-      // Confirm player 1 false click
-      await page.keyboard.press('e');
-      await expect(cells.nth(2)).toHaveText('');
-    }
-  });
-
-  test('Should ensure sybols cannot be placed after a game is complete', async ({ page }) => {
-    const cells = page.locator('.board-cell');
-
-    // Detect which player was randomly selected to go first
-    await cells.nth(0).click();
-    const firstSymbol = (await cells.nth(0).textContent())!;
-    const secondSymbol = firstSymbol === 'X' ? 'O' : 'X';
-
-    // Play a winning game scenario
-    await cells.nth(3).click();
-    await cells.nth(1).click();
-    await cells.nth(4).click();
-    await cells.nth(2).click();
-
-    // Check placement in empty cell remains empty
-    await cells.nth(5).click();
-    await expect(cells.nth(5)).toHaveText('');
-  });
-
+  // Check placement in empty cell remains empty
+  await cells.nth(5).click();
+  await expect(cells.nth(5)).toHaveText('');
 });
--- e2e/player.spec.ts
+++ e2e/player.spec.ts
@@ -1,116 +1,112 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Tic Attack Toe App - Player Details', () => {
+test.beforeEach(async ({ page }) => {
+  // Open page
+  await page.goto('http://127.0.0.1:5500');
 
-  test.beforeEach(async ({ page }) => {
-    // Open page
-    await page.goto('http://127.0.0.1:5500');
+  // Click on the begin button
+  await page.click('#button-begin');
+});
 
-    // Click on the begin button
-    await page.click('#button-begin');
-  });
+test('Should limit player 1 name to 16 characters', async ({ page }) => {
+  // Enter 17 character player 1 name
+  await page.waitForTimeout(600);
+  await page.fill('#player1-name-input', '12345678901234567');
 
-  test('Should limit player 1 name to 16 characters', async ({ page }) => {
-    // Enter 17 character player 1 name
-    await page.waitForTimeout(600);
-    await page.fill('#player1-name-input', '12345678901234567');
+  // Verify player 1 name is 16 characters.
+  const playerName = await page.inputValue('#player1-name-input');
+  expect(playerName.length).toBe(16);
+});
 
-    // Verify player 1 name is 16 characters.
-    const playerName = await page.inputValue('#player1-name-input');
-    expect(playerName.length).toBe(16);
-  });
+test('Should limit player 2 name to 16 characters', async ({ page }) => {
+  // Enter player 1 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.click('#player1-colors .color-circle.bg-plum');
+  
+  // Enter 17 character player 2 name
+  await page.waitForTimeout(600);
+  await page.fill('#player2-name-input', '12345678901234567');
 
-  test('Should limit player 2 name to 16 characters', async ({ page }) => {
-    // Enter player 1 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.click('#player1-colors .color-circle.bg-plum');
-    
-    // Enter 17 character player 2 name
-    await page.waitForTimeout(600);
-    await page.fill('#player2-name-input', '12345678901234567');
+  // Verify player 1 name is 16 characters.
+  const playerName = await page.inputValue('#player2-name-input');
+  expect(playerName.length).toBe(16);
+});
 
-    // Verify player 1 name is 16 characters.
-    const playerName = await page.inputValue('#player2-name-input');
-    expect(playerName.length).toBe(16);
-  });
+test('Should leave continue button disabled if player 1 name is empty', async ({ page }) => {
+  // Select player 1 color
+  await page.waitForTimeout(600);
+  await page.click('#player1-colors .color-circle.bg-plum');
 
-  test('Should leave continue button disabled if player 1 name is empty', async ({ page }) => {
-    // Select player 1 color
-    await page.waitForTimeout(600);
-    await page.click('#player1-colors .color-circle.bg-plum');
+  // Verify button remains disabled
+  await expect(page.locator('#button-next-player1')).toBeDisabled();
+});
 
-    // Verify button remains disabled
-    await expect(page.locator('#button-next-player1')).toBeDisabled();
-  });
+test('Should leave continue button disabled if player 2 name is empty', async ({ page }) => {
+  // Enter player 1 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.click('#player1-colors .color-circle.bg-plum');
+  
+  // Select player 2 color
+  await page.waitForTimeout(600);
+  await page.click('#player2-colors .color-circle.bg-peach');
 
-  test('Should leave continue button disabled if player 2 name is empty', async ({ page }) => {
-    // Enter player 1 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.click('#player1-colors .color-circle.bg-plum');
-    
-    // Select player 2 color
-    await page.waitForTimeout(600);
-    await page.click('#player2-colors .color-circle.bg-peach');
+  // Verify button remains disabled
+  await expect(page.locator('#button-next-player2')).toBeDisabled();
+});
 
-    // Verify button remains disabled
-    await expect(page.locator('#button-next-player2')).toBeDisabled();
-  });
+test('Should enter player details and move to game mode screen (name then color)', async ({ page }) => {
+  // Enter player 1 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.click('#player1-colors .color-circle.bg-plum');
 
-  test('Should enter player details and move to game mode screen (name then color)', async ({ page }) => {
-    // Enter player 1 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.click('#player1-colors .color-circle.bg-plum');
+  // Enter player 2 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player2-name-input', 'Player 2');
+  await page.click('#player2-colors .color-circle.bg-peach');
 
-    // Enter player 2 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player2-name-input', 'Player 2');
-    await page.click('#player2-colors .color-circle.bg-peach');
+  // Verify the transition to game mode screen
+  await expect(page.locator('#button-start-zen')).toBeVisible();
+  await expect(page.locator('#button-start-battle')).toBeVisible();
+});
 
-    // Verify the transition to game mode screen
-    await expect(page.locator('#button-start-zen')).toBeVisible();
-    await expect(page.locator('#button-start-battle')).toBeVisible();
-  });
+test('Should enter player details and move to game mode screen (color then name)', async ({ page }) => {
+  // Enter player 1 details after animation
+  await page.waitForTimeout(600);
+  await page.click('#player1-colors .color-circle.bg-plum');
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.waitForTimeout(600);
+  await page.click('#button-next-player1');
 
-  test('Should enter player details and move to game mode screen (color then name)', async ({ page }) => {
-    // Enter player 1 details after animation
-    await page.waitForTimeout(600);
-    await page.click('#player1-colors .color-circle.bg-plum');
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.waitForTimeout(600);
-    await page.click('#button-next-player1');
+  // Enter player 2 details after animation
+  await page.waitForTimeout(600);
+  await page.click('#player2-colors .color-circle.bg-peach');
+  await page.fill('#player2-name-input', 'Player 2');
+  await page.waitForTimeout(600);
+  await page.click('#button-next-player2');
 
-    // Enter player 2 details after animation
-    await page.waitForTimeout(600);
-    await page.click('#player2-colors .color-circle.bg-peach');
-    await page.fill('#player2-name-input', 'Player 2');
-    await page.waitForTimeout(600);
-    await page.click('#button-next-player2');
+  // Verify the transition to game mode screen
+  await expect(page.locator('#button-start-zen')).toBeVisible();
+  await expect(page.locator('#button-start-battle')).toBeVisible();
+});
 
-    // Verify the transition to game mode screen
-    await expect(page.locator('#button-start-zen')).toBeVisible();
-    await expect(page.locator('#button-start-battle')).toBeVisible();
-  });
+test('Should select game mode and proceed to countdown', async ({ page }) => {
+  // Enter player 1 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player1-name-input', 'Player 1');
+  await page.click('#player1-colors .color-circle.bg-plum');
 
-  test('Should select game mode and proceed to countdown', async ({ page }) => {
-    // Enter player 1 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player1-name-input', 'Player 1');
-    await page.click('#player1-colors .color-circle.bg-plum');
+  // Enter player 2 details after animation
+  await page.waitForTimeout(600);
+  await page.fill('#player2-name-input', 'Player 2');
+  await page.click('#player2-colors .color-circle.bg-peach');
 
-    // Enter player 2 details after animation
-    await page.waitForTimeout(600);
-    await page.fill('#player2-name-input', 'Player 2');
-    await page.click('#player2-colors .color-circle.bg-peach');
+  // Select zen mode
+  await page.waitForTimeout(600);
+  await page.click('#button-start-zen');
 
-    // Select zen mode
-    await page.waitForTimeout(600);
-    await page.click('#button-start-zen');
-
-    // Verify the transition to countdown screen
-    await expect(page.locator('#countdown-timer')).toBeVisible();
-  });
-
+  // Verify the transition to countdown screen
+  await expect(page.locator('#countdown-timer')).toBeVisible();
 });
