b7117a05bb741ac6a165b732505eceb99649a4cb
Brett Field <brett@digitalclarity.co>
2024-07-28 19:48:48 +0200

    Playwright config update.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const isCI = !!process.env.CI;
-const baseURL = isCI ? 'https://bcfield.github.io/tic-attack-toe' : 'http://127.0.0.1:5500';
-
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
@@ -11,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL,
+    baseURL: 'http://127.0.0.1:5500',
     trace: 'on-first-retry',
   },
   projects: [
@@ -19,21 +16,18 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
-  webServer: isCI
-    ? undefined
-    : {
-        command: 'serve dist',
-        url: 'http://127.0.0.1:5500',
-        reuseExistingServer: !process.env.CI,
-        timeout: 120 * 1000,
-      },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
